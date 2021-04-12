1c5c0fb90dfc9fbf6cbed7ba37f2d5c56b14704a
codemobiles <chaiyasit.t@gmail.com>
2021-04-12 11:13:19 +0700

    CMFastAPI Workshop 7 Path Containing File Path Ep3

---
+++ cm_fast_api_demo8/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/items/{item_id}")
+def read_item(item_id:int):
+    return {"item_id": item_id}
+
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
