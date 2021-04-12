859a86296c8fa50f354c055beda4c17e05c60298
codemobiles <chaiyasit.t@gmail.com>
2021-04-12 19:31:30 +0700

    CMFastAPI Workshop 16 Additional Response Status Code

--- cm_fast_api_demo8/main.py
+++ cm_fast_api_demo8/main.py
@@ -5,8 +5,7 @@ app = FastAPI()
 
 
 @app.get("/items/")
-
-async def read_items(q: Optional[str] = Query(None, min_length=3, max_length=5)):
+async def read_items(q: Optional[str] = Query(None, min_length=0, max_length=50, regex="[abc]")):
 
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
